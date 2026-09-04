bbab7332c4038eb6ca6187d4b79b85a015d2f624
AmaneYume <84631273+dream10325@users.noreply.github.com>
2026-09-05 00:01:37 +0800

    新增更新紀錄

---
+++ js/changelog.js
@@ -0,0 +1,34 @@
+const CHANGELOG_DATA = [
+  {
+    version: "v1.1.0",
+    date: "2026-09-04",
+    logs: [
+      "我們有酷酷的log播放器了"
+    ]
+  },
+];
+
+(function () {
+  function renderChangelog() {
+    const list = document.getElementById('changelogList');
+    if (!list) return;
+
+    list.innerHTML = CHANGELOG_DATA.map(item => `
+      <div class="changelog-item">
+        <div class="changelog-header">
+          <span class="changelog-ver">${item.version}</span>
+          <span class="changelog-date">${item.date}</span>
+        </div>
+        <ul class="changelog-entries">
+          ${item.logs.map(log => `<li>${log}</li>`).join('')}
+        </ul>
+      </div>
+    `).join('');
+  }
+
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', renderChangelog);
+  } else {
+    renderChangelog();
+  }
+})();
